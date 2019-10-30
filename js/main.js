42d272589b89b46c0507756ed7765430decac95c
day1park <daypark2408@gmail.com>
2019-10-30 12:49:13 +1100

    add JQuery CDN and smooth scroll function

--- js/main.js
+++ js/main.js
@@ -11,3 +11,19 @@ function initMap() {
   // the marker, positioned at location
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// smooth scroll
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      650
+    );
+  }
+});
